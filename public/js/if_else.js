ce0ae32f455dbb2c8fa68c9405b99083f1666322
Andrew Powell-Morse <andrewpomo815@gmail.com>
2016-03-16 12:55:24 -0500

    Created JavaScript If/Else If/Else statement to check for the presence of randomly-appearing "colors" and log a brief description of the color that appears in the console.
    Made a ternary operator to log a special message when the randomly-appearing color is the one I marked as my favorite!

---
+++ public/js/if_else.js
@@ -0,0 +1,66 @@
+"use strict"
+// ignore these lines for now
+// just know that the variable 'color' will end up with a random value from the colors array
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+var color = colors[Math.floor(Math.random()*colors.length)];
+
+var favorite = 'blue'; // TODO: change this to your favorite color from the list
+
+
+// For the assignment, this is wrong. I still think it it's cool, though.
+
+// if (colors.indexOf('red') > -1){
+// 	console.log('Red is here. It is the color of stop signs.')
+// }
+
+// if (colors.indexOf('orange') > -1){
+// 	console.log('Orange is here. It is the color of oranges.')
+// }
+
+// if (colors.indexOf('yellow') > -1){
+// 	console.log('Yellow is here. It is the color of lemons.')
+// }
+
+// if (colors.indexOf('green') > -1){
+// 	console.log('Green is here. It the color of grass.')
+// }
+
+// if (colors.indexOf('blue') > -1){
+// 	console.log('Blue is here. It the color of the best snow cone flavor.')
+// }
+
+// if (colors.indexOf('Indigo') > -1){
+// 	console.log('Indigo is here, but I don't know anything by that color.)
+// }
+
+// if (colors.indexOf('violet') > -1){
+// 	console.log('Violet is here, but I don't know anything by that color.')
+// }
+
+if (color == 'red') {
+	console.log('This is red - the color of apples.')
+} else if (color == 'orange') {
+	console.log('This is orange - the color of oranges.')
+} else if (color == 'yellow') {
+	console.log('This is yellow - the color of lemons.')
+} else if (color == 'green') {
+	console.log('This is green - the color of limes.')
+} else if (color == 'blue') {
+	console.log('This is blue - the color of coconu...wait...')
+} else {
+	console.log('This is either indigo or violet (Can you tell them apart? I can\'t.). I don\'t know anything by either of these colors.')
+} 
+
+var favColor = color == favorite ? ("That's why it's my favorite color.") : ("But it's not my favorite color.")
+
+console.log(favColor)
+
+// TODO: Create a block of if/else statements to check for every color except indigo and violet.
+// TODO: When a color is encountered log a message that tells the color, and an object of that color.
+//       Example: Blue is the color of the sky.
+
+// TODO: Have a final else that will catch indigo and violet.
+// TODO: In the else, log: I do not know anything by that color.
+
+// TODO: Using the ternary operator, conditionally log a statement that
+//       says whether the random color matches your favorite color.
