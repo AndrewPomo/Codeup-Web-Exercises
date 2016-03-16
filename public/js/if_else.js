a22f9d0e37fef5d5d4eedbe86ec575711981a12b
Andrew Powell-Morse <andrewpomo815@gmail.com>
2016-03-16 14:22:32 -0500

    Made more text changes to if_else.js
    Created switch_statement_js.html, which uses an inline script to run a switch statement. The statement does essentially the same thing as the if_else.js statement, but is simpler.

--- public/js/if_else.js
+++ public/js/if_else.js
@@ -29,26 +29,6 @@ var favorite = 'blue'; // TODO: change this to your favorite color from the list
 // 	console.log('Blue is here. It is the color of the best snow cone flavor.')
 // }
 
-if (color == 'red') {
-	console.log('This is red - the color of apples.')
-} else if (color == 'orange') {
-	console.log('This is orange - the color of oranges.')
-} else if (color == 'yellow') {
-	console.log('This is yellow - the color of lemons.')
-} else if (color == 'green') {
-	console.log('This is green - the color of limes.')
-} else if (color == 'blue') {
-	console.log('This is blue - the color of the objectively best snow cone flavor.')
-} else {
-	console.log('This is either indigo or violet (Can you tell them apart? I can\'t.). I don\'t know anything by either of these colors.')
-} 
-
-console.log(color == favorite ? "That's why it's my favorite color." : "But it's not my favorite color.")
-
-// var favColor 
-
-// console.log(favColor)
-
 // TODO: Create a block of if/else statements to check for every color except indigo and violet.
 // TODO: When a color is encountered log a message that tells the color, and an object of that color.
 //       Example: Blue is the color of the sky.
@@ -56,5 +36,24 @@ console.log(color == favorite ? "That's why it's my favorite color." : "But it's
 // TODO: Have a final else that will catch indigo and violet.
 // TODO: In the else, log: I do not know anything by that color.
 
+if (color == 'red') {
+	console.log('This is red - the color of apples.');
+} else if (color == 'orange') {
+	console.log('This is orange - the color of oranges.');
+} else if (color == 'yellow') {
+	console.log('This is yellow - the color of lemons.');
+} else if (color == 'green') {
+	console.log('This is green - the color of limes.');
+} else if (color == 'blue') {
+	console.log('This is blue - the color of the objectively best snow cone flavor.');
+} else {
+	console.log('I do not know anything by that color.');
+} 
+
 // TODO: Using the ternary operator, conditionally log a statement that
 //       says whether the random color matches your favorite color.
+console.log(color == favorite ? "That's why it's my favorite color." : "But it's not my favorite color.");
+
+
+
+
