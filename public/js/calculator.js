e7b6fa1ae162e10d0112777129b23762400546cd
Andrew Powell-Morse <andrewpomo815@gmail.com>
2016-03-24 15:23:18 -0500

    Did a whole bunch of stuff to the calculator without committing anything. It is done now. oops.

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -10,72 +10,135 @@
 
 
 
- 	//Fields
+//Fields
  	var leftOperand = document.getElementById('left_operand');
  	var rightOperand = document.getElementById('right_operand');
  	var operatorField = document.getElementById('operator');    	
- 	
+ 	var displayField = document.getElementById('display_field');    	
 
- 	
- 	//Operator Buttons
+//Operator Buttons
  	var addBtn = document.getElementById('addBtn');
  	var subtractBtn = document.getElementById('subtractBtn');
  	var multiplyBtn = document.getElementById('multiplyBtn');
  	var divideBtn = document.getElementById('divideBtn');
+//Equals Button
  	var equalsBtn = document.getElementById('equalsBtn');
+
+//Clear Button
  	var clearBtn = document.getElementById('clearBtn');
 
- 	// //Number Buttons
- 	// var zeroBtn = document.getElementById('zeroBtn')
- 	// var oneBtn = document.getElementById('oneBtn')
- 	// var twoBtn = document.getElementById('twoBtn')
- 	// var threeBtn = document.getElementById('threeBtn')
- 	// var fourBtn = document.getElementById('fourBtn')
- 	// var fiveBtn = document.getElementById('fiveBtn')
- 	// var sixBtn = document.getElementById('sixBtn')
- 	// var sevenBtn = document.getElementById('sevenBtn')
- 	// var eightBtn = document.getElementById('eightBtn')
- 	// var nineBtn = document.getElementById('nineBtn')
-
- 	 // Get all number buttons
+//Insert Number Function
  	var insNumber = function (event) { 
-    	var operatorField = document.getElementById('operator');    	
-    	if (operatorField.value == "Operator" && leftOperand.value == "First #") {
+    	if (operatorField.value == "" && leftOperand.value == "") {
     		leftOperand.value = this.value;
-	    } else if (operatorField.value == "Operator" && leftOperand.value != "First #") {
+    		displayField.value = this.value;
+	    } else if (operatorField.value == "" && leftOperand.value != "") {
 	    	leftOperand.value = leftOperand.value + this.value;
+	    	displayField.value = leftOperand.value;
+	    } else if (operatorField.value != "" && leftOperand.value != "" && rightOperand.value == "") {
+	    	rightOperand.value = this.value;
+	    	displayField.value = rightOperand.value;
+		} else if (operatorField.value != "" && leftOperand.value != "" && rightOperand.value != "") {
+	    	rightOperand.value = rightOperand.value + this.value;
+	    	displayField.value = rightOperand.value;
 	    }
-    }
+    }	
 
+//Insert Operator Function
     var insOperator = function (event) { 
-    	var leftOperand = document.getElementById('left_operand');
- 		var rightOperand = document.getElementById('right_operand');
     	if (leftOperand.value != "First #") {
     		operatorField.value = this.value;
+    		displayField.value = leftOperand.value;
 	    } 
     }
+
+//Calculate Function
+	var calculate = function (event) { 
+		var rightNumber = parseFloat(rightOperand.value);
+		var leftNumber = parseFloat(leftOperand.value);
+
+		if (leftOperand.value == "First #" || rightOperand.value == "Second #" || operatorField.value == "Operator") {
+    		alert("You need a first number, an operator and a second number in order to use the equals sign!");
+    	}else if(operatorField.value == "+") {
+    		var answer = leftNumber + rightNumber;
+    	}else if(operatorField.value == "-") {
+    		var answer = leftNumber - rightNumber;
+    	}else if(operatorField.value == "*") {
+    		var answer = leftNumber * rightNumber;
+    	}else if(operatorField.value == "/") {
+    		var answer = leftNumber / rightNumber;
+    	}
+		displayField.value = answer;
+	}
+
+	//Clear all
+	var clear = function (event) { 
+		leftOperand.value = "";
+		rightOperand.value = "";
+		operatorField.value = "";
+		displayField.value = "";
+	}
+    		
+	// Positive/Negative
+	var changePosNeg = function (event) { 
+	    if (operatorField.value == "" && leftOperand.value != "") {
+	    	leftOperand.value = (leftOperand.value * -1);
+	    	displayField.value = leftOperand.value;
+	    } else if (operatorField.value != "" && leftOperand.value != "" && rightOperand.value == "") {
+	    	rightOperand.value = (rightOperand.value * -1);
+	    	displayField.value = rightOperand.value;
+		} else if (operatorField.value != "" && leftOperand.value != "" && rightOperand.value != "") {
+	    	(rightOperand.value = rightOperand.value * -1);
+	    	displayField.value = rightOperand.value;
+	    }
+    }	
+
+    // Positive/Negative
+	var percentify = function (event) { 
+	    if (operatorField.value == "" && leftOperand.value != "") {
+	    	leftOperand.value = (leftOperand.value * .01);
+	    	displayField.value = leftOperand.value;
+	    } else if (operatorField.value != "" && leftOperand.value != "" && rightOperand.value == "") {
+	    	rightOperand.value = (rightOperand.value * .011);
+	    	displayField.value = rightOperand.value;
+		} else if (operatorField.value != "" && leftOperand.value != "" && rightOperand.value != "") {
+	    	(rightOperand.value = rightOperand.value * .01);
+	    	displayField.value = rightOperand.value;
+	    }
+    }	
+    	
 
     // Add Click Listener To Number Buttons (And Log Their Values)
     var numButtons = document.getElementsByClassName('num_button');
 	    for(var i = 0; i < numButtons.length; i++) {
     	numButtons[i].addEventListener('click', insNumber);
-    	console.log(numButtons[i].value);
+    	// console.log(numButtons[i].value);
     	}
 
     // // Add Click Listener To Operator Buttons
     var opButtons = document.getElementsByClassName('op_button');
 	    for(var i = 0; i < opButtons.length; i++) {
     	opButtons[i].addEventListener('click', insOperator);
-    	console.log(opButtons[i].value);
+    	// console.log(opButtons[i].value);
     	}
 
     // // Add Click Listener To Equals Button
-    //  var numButton = document.getElementsByClassName('equals_button');
-    //         numButton.addEventListener('click', calculate);
+    var equalsBtn = document.getElementById('equalsBtn');
+    	equalsBtn.addEventListener('click', calculate);
 
     // // Add Click Listener To Clear Button
-    //  var numButton = document.getElementsByClassName('clear_button');
-    //         numButton.addEventListener('click', clear);
+    var clearBtn = document.getElementById('clearBtn');
+    	clearBtn.addEventListener('click', clear);
+
+    // Add Click Listener To PosNeg Button
+	var posNegButton = document.getElementById('pos_neg');
+        posNegButton.addEventListener('click', changePosNeg);
+
+    // Add Click Listener To Percent Button
+    var percentButton = document.getElementById('percent');
+        percentButton.addEventListener('click', percentify);
+
+   
 
    
 
