f9a5492e4c797c00e1d0a87280efebf52d1536b3
Andrew Powell-Morse <andrewpomo815@gmail.com>
2016-04-27 11:21:48 -0500

    Created file and UN/PSWD form.

---
+++ public/login.php
@@ -0,0 +1,18 @@
+<?php
+var_dump($_POST);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>POST</title>
+</head>
+<body>
+    <form method="POST">
+        <label>Username</label>
+        <input type="text" name="userName"><br>
+        <label>Password</label>
+        <input type="password" name="password"><br>
+        <input type="submit">
+    </form>
+</body>
+</html>
