af0e4bb5ee213a0c0dccd6acb1448fedf6aa2821
Andrew Powell-Morse <andrewpomo815@gmail.com>
2016-05-02 09:26:15 -0500

    Templated site navbar

--- public/navbar.php
+++ public/navbar.php
@@ -1,38 +1,53 @@
-<div id="main-nav" class="navbar navbar-fixed-top">
-		<div class="container">
-		
-			<div class="navbar-header">
-			
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#site-nav">
-					<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
-				</button>
-				
-				<!-- ======= LOGO ========-->
-				<a class="navbar-brand scrollto" href="#home">ANDREW POWELL-MORSE<br><span>Person</span></a>
-				<!-- <a class="navbar-brand scrollto ir-logo" href="#">ANDREW POWELL-Morse<br><span>Person</span></a> -->
-			
-			</div>
-			
-			<div id="site-nav" class="navbar-collapse collapse">
-				<ul class="nav navbar-nav navbar-right">
-					<li class="sr-only">
-						<a href="#home" class="scrollto">Home</a>
-					</li>
-					<li>
-						<a href="#services" class="scrollto">What I do</a>
-					</li>
-					<li>
-						<a href="#about" class="scrollto">About Me</a>
-					</li>
-					<li>
-						<a href="#portfolio" class="scrollto">My Works</a>
-					</li>
-					<li>
-						<a href="#contact" class="scrollto">Contact Me</a>
-					</li>
-				</ul>
-			</div><!--End navbar-collapse -->
-			
-		</div><!--End container -->
-		
-	</div><!--End main-nav -->";
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Brand</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="navbar-form navbar-left" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
